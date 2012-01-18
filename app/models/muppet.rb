04374e445be69ae35afdcfc05d4ac8e0392f9ec8
David Vollbracht <david.vollbracht@gmail.com>
2012-01-18 10:09:49 -0500

    Rename city to location

--- app/models/muppet.rb
+++ app/models/muppet.rb
@@ -3,7 +3,7 @@ class Muppet
 
   field :name, type: String
   field :color, type: String
-  field :city, type: String
+  field :location, type: String, default: -> { read_attribute :city }
 
   def to_s
     name
